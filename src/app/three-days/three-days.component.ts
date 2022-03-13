0a3e0991224e758fe805c22ab71a0ce3ead464f2
Natalia Norboeva <divonata@gmail.com>
2022-03-13 16:17:44 +0900

    removed ThreeDaysComponent

--- src/app/three-days/three-days.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-three-days',
-  templateUrl: './three-days.component.html',
-  styleUrls: ['./three-days.component.scss'],
-})
-export class ThreeDaysComponent {
-}
